c27eb44c31891e5a5d2887e77e74f11937f5a258
Wander Feliz <55289565+WanderFeliz-Octagono@users.noreply.github.com>
2021-07-18 11:08:56 -0400

    config api/date route 1 and 2 condition

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+let cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", function (req, res) {
+  let {date} = req.params;
+  let dateVal = new Date(date);
+  let utcDate = new Date();
+  let unixDate = "";
+  unixDate = Date.parse(date)
+  console.log(typeof(unixDate))
+  if (isNaN(unixDate)){
+    utcDate.setTime(date)
+    unixDate = Date.parse(utcDate);
+  }else{
+    utcDate.setTime(dateVal)
+  }
+  res.json({unix: unixDate, utc: utcDate.toUTCString()});
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+process.env.PORT = 3000;
+let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
